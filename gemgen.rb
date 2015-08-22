548134537b2d8e29e5bd9885562096e18299e9c9
Muhammadreza Haghiri <haghiri75@gmail.com>
2015-08-22 22:56:53 +0430

    Script

---
+++ gemgen.rb
@@ -0,0 +1,33 @@
+puts "Welcome to GEM File Generator"
+
+file = ARGV[0]
+
+n = 0
+gems = []
+File.open(file, 'r') do |ln|
+while line = ln.gets
+ line["\n"] = ""
+if line[0] != "#"
+ if /[require\s\'a-zA-Z0-9\']/ === line
+  line = line.split('require')
+  gems[n] = line
+  n += 1
+ end
+end
+end
+end
+
+m = 0
+
+gemfile = File.new('Gemfile', 'w+')
+
+gemfile.write("source 'https://rubygems.org'\n")
+
+while m < File.open('Gemfile', 'r') {|n| n.count}
+ gemfile.write("#{gems[m]}\n")
+ puts "Line number #{m} has been written"
+end
+  
+ 
+
+
